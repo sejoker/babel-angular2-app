8b96579e84235260faf2589a1e1c78188cb0b2a0
Yevgen Safronov <sejoker@gmail.com>
2016-09-04 20:29:46 +0200

    integrate redux

--- src/app.js
+++ src/app.js
@@ -3,6 +3,17 @@ import { LocationStrategy, HashLocationStrategy } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { ActivatedRoute, RouterModule } from '@angular/router';
 import 'rxjs/add/operator/map';
+import {
+  applyMiddleware,
+  Store,
+  combineReducers,
+  compose,
+  createStore
+} from 'redux';
+import { NgRedux, select } from 'ng2-redux';
+import createLogger from 'redux-logger';
+import { rootReducer } from './reducers';
+import { CounterActions } from './actions/counterActions';
 
 import { Greeter } from './services';
 
@@ -40,11 +51,33 @@ export class Linker {
 }
 
 @Component({
+  selector: 'counter',
+  providers: [ CounterActions],
+  template: `
+  <p>
+    Clicked: {{ counter$ | async }} times
+    <button (click)="actions.increment()">+</button>
+    <button (click)="actions.decrement()">-</button>
+    <button (click)="actions.incrementIfOdd()">Increment if odd</button>
+    <button (click)="actions.incrementAsync(2222)">Increment async</button>
+  </p>
+  `
+})
+export class Counter {
+  @select('counter') counter$: Observable<number>;
+
+  constructor(actions: CounterActions) {
+    this.actions = actions;
+  }
+}
+
+@Component({
   selector: 'hello-app',
   template: `
     <ul>
       <li><a [routerLink]="['/']">Hello</a></li>
       <li><a [routerLink]="['/ciao', 'ng2']">Ciao</a></li>
+      <li><a [routerLink]="['/counter']">Counter</a></li>
     </ul>
     <router-outlet></router-outlet>
     <linker name="GitHub" url="https://github.com/shuhei/babel-angular2-app"></linker>
@@ -56,6 +89,7 @@ export class HelloApp {
 const routing = RouterModule.forRoot([
   { path: '', component: Hello },
   { path: 'ciao/:name', component: Ciao },
+  { path: 'counter', component: Counter },
 ]);
 
 @NgModule({
@@ -68,12 +102,23 @@ const routing = RouterModule.forRoot([
     Hello,
     Ciao,
     Linker,
+    Counter
   ],
   providers: [
+    NgRedux,
     Greeter,
+    CounterActions,
     { provide: LocationStrategy, useClass: HashLocationStrategy },
   ],
   bootstrap: [HelloApp],
 })
 export class AppModule {
+  constructor(ngRedux: NgRedux) {
+    ngRedux.configureStore(rootReducer, {}, [ createLogger() ]);
+  }
 }
+
+// const store = createStore(
+//  //rootReducer,
+//  function() {},
+//  compose(applyMiddleware(reduxLogger)));
---
+++ src/reducers/counter.js
@@ -0,0 +1,17 @@
+import { CounterActions } from '../actions/counterActions';
+
+const INITIAL_STATE = 0;
+
+export function counterReducer(state = INITIAL_STATE, action) {
+
+  switch (action.type) {
+    case CounterActions.INCREMENT_COUNTER:
+      return state + 1;
+    case CounterActions.DECREMENT_COUNTER:
+      return state - 1;
+    case CounterActions.RANDOMIZE_COUNTER:
+      return action.payload;
+    default:
+      return state;
+  }
+}
